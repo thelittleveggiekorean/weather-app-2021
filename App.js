3adabed3262ac871824e3bdaa02c871018e2b642
Chohee <chohee4421@gmail.com>
2021-10-05 13:26:06 +0900

    Updated styles for weather app

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text, ScrollView, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+
+
+const {width: SCREEN_WIDTH} = Dimensions.get('window');
 
 export default function App() {
+  // const [location, setLocation] = useState(); 
+  // const [ok, setOk] = useState(true);
+  // const ask = async() => {
+  //   const permission = await Location.requestForegroundPermissionsAsync();
+  // }
+
+  // useEffect(()=>{
+  //   ask();
+  // }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Seoul</Text>
+        </View>
+        <ScrollView showsHorizontalScrollIndicator={false} pagingEnabled={true} contentContaienrStyle={styles.weather} horizontal={true} >
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>sunny</Text>
+          </View>
+        </ScrollView>
+      </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    backgroundColor: "coral"
   },
-});
+  city: {
+    flex:1.6,
+    justifyContent:"center",
+    alignItems: "center"
+  },
+  cityName: {
+    fontSize: 48,
+    fontWeight: "500"
+  },
+  weather: {
+    backgroundColor:"gray"
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    flex:1,
+    alignItems: "center"
+  },
+  temp: {
+    marginTop:50,
+    fontSize:148,
+    fontWeight: "500"
+  },
+  description: {
+    marginTop:-20,
+    fontSize:68
+  }
+})
